8fe485901d49bdaecbab1460bd08907776ccf812
harsh kumar garg <83538674+harshKumargarg@users.noreply.github.com>
2021-05-21 21:33:49 +0530

    Create  harsh decision control if.c

---
+++ harsh decision control if.c
@@ -0,0 +1,18 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+    int main() {
+        int x;
+    printf("enter a number" );
+    scanf("%d",&x);
+    if (x>0)
+    {
+    printf("positive number");
+    }
+    if (x<=0)
+    {
+    
+    printf("non positive number");
+    }
+    return 0;
+}
