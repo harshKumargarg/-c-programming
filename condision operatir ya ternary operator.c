ffb7077f337a4721088abc407539934fce6b61ad
harsh kumar garg <83538674+harshKumargarg@users.noreply.github.com>
2021-05-22 23:06:18 +0530

    Create condision operatir ya ternary operator.c

---
+++ condision operatir ya ternary operator.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+  int x;
+  printf("enter a number" );
+  scanf("%d",&x);  
+  (x>0) ?  printf("positive")  : printf("non positive");
+ 
+   
+    return 0;
+}
