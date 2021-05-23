8e4d4b6c70c05cbd5f9a382bfb327a042a60c654
harsh kumar garg <83538674+harshKumargarg@users.noreply.github.com>
2021-05-23 14:20:30 +0530

    Create loops while.c

---
+++ loops while.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+  int i=1;
+  
+  while(i<=9)
+  {
+      printf( "\n harshu");
+      i++;
+  }  
+    return 0;
+}
